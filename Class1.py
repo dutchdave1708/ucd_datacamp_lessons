f901056c4f89e6deb84eba15e5d09cc2b7d57167
dutchdave1708 <d_j_pit@yahoo.com>
2021-09-13 22:02:07 +0100

    Checking in file for Week 1 lesson in my new GitHub repo

---
+++ Class1.py
@@ -0,0 +1,21 @@
+# Class 1
+# introductions
+# installations of PyCharm
+# Basic functions like print and calculations
+# Calculations like #multiply (mul), add (+), divide (div), subtract (sub), ** (to the power of)
+#print(a+b)
+#print(sub)
+#print(div)
+#print(mul)
+5-2
+print(5-2)
+answer = 3**2
+print(answer)
+
+# Completed session 1 on DataCamp ~5 hours // Introduction to Python
+
+#functions: built in like max(), len() on a list
+#method: functions belong to objects
+#packages : Numpy is a Python package to import, to effiently practise data science
+# for example: list named fam.  object.method()  --> fam.index("word3")
+
