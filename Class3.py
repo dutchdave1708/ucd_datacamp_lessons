c1cdf3d17f5ee35207a746e7549571d8a1883dcd
dutchdave1708 <d_j_pit@yahoo.com>
2021-09-13 22:07:49 +0100

    Checking in file for Week 3 lesson in my new GitHub repo

---
+++ Class3.py
@@ -0,0 +1,9 @@
+# class 3
+# for <any name> in enumerate(list) --> the enumerate means you also access the index data
+# installed Git on macbook via Homebrew
+# install Homebrew dave$ /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# install Git : Daves-MacBook-Air:~ dave$ $ brew install git
+# create GitHub account
+# update PyCharm to link to GitHub account
+# link PyCharm project to GitHub repo
+# commit&push the existing files to GitHub
